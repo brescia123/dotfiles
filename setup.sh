72c5fe3fae7704d65f5d73e4169c0a258dc3a82a
Giacomo Bresciani <brescia123@gmail.com>
2016-01-14 09:58:00 +0100

    add brew, git and autojump

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
+# Git
+which -s git || brew install git
+
+# Autojump
+brew install autojump
+
 # Creating hard links to the home folder; using -f to replace existing links
 ln -f .git-prompt.sh ~/.git-prompt.sh
 ln -f .git-completion.bash ~/.git-completion.bash

5e5a972786792fd8a58238363588b4b7f9cbd9fe
Giacomo Bresciani <brescia123@users.noreply.github.com>
2020-09-14 07:33:42 +0000

    Update homebrew script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else
     brew update
 fi
